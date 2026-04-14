645954f6126d508bad44fedd4f77602d0e445024
Xopher <xopher@vi.co>
2026-04-14 04:19:36 -0700

    build: update production assets and manifest for Xophz-COMPASS admin distribution

---
+++ admin/dist/js/lead-magnet.api-BZagGXKL.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-CVfH8lqt.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/magic-formula.api-CjXlrWvh.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-CVfH8lqt.js";const e=()=>o.get("get_hustle_modules");export{e as g};
