41abc623e4a32bcf9affae8242228dc5855e642e
Xopher <xopher@vi.co>
2026-04-15 05:34:30 -0700

    chore: update build assets and manifest for plugin sub-app routes and APIs

---
+++ admin/dist/js/lead-magnet.api-B5gfR_xA.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-Bb8oXP5I.js";const s=()=>e.get("get_hustle_modules");export{s as g};
--- admin/dist/js/magic-formula.api-DvXQSmkg.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-Ik8pHtr4.js";const e=()=>o.get("get_hustle_modules");export{e as g};
