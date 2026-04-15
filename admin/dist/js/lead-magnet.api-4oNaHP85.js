bc2944ed73daa3faa9401261649c86019ffa3b67
Xopher <xopher@vi.co>
2026-04-15 11:42:19 -0700

    chore: update admin build assets and manifest with new hashed filenames

---
+++ admin/dist/js/lead-magnet.api-4oNaHP85.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-Cx9HyL-_.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/magic-formula.api-CU-Wk9NE.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-Cx9HyL-_.js";const e=()=>o.get("get_hustle_modules");export{e as g};
