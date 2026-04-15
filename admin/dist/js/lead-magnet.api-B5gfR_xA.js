d546300a5cb34b5a8af519af5245cedb4dc56b15
Xopher <xopher@vi.co>
2026-04-15 07:02:36 -0700

    chore: update build artifacts and manifest for admin dashboard assets

--- admin/dist/js/lead-magnet.api-B5gfR_xA.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-Bb8oXP5I.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/lead-magnet.api-CAUV49ng.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-BT6aGq5c.js";const s=()=>e.get("get_hustle_modules");export{s as g};
