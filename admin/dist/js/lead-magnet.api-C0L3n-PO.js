e6336b08c7ba4ee03c931be5165b1569862ae91a
Xopher <x@hallofthegods.com>
2026-04-25 07:00:57 -0700

    chore: update admin distribution JS assets by rebuilding and reorganizing bundles

---
+++ admin/dist/js/lead-magnet.api-C0L3n-PO.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-B7SQRei5.js";const s=()=>e.get("get_hustle_modules");export{s as g};
--- admin/dist/js/lead-magnet.api-DhhvRO_h.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-DdobNb9z.js";const s=()=>e.get("get_hustle_modules");export{s as g};
