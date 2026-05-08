71f8fd07c0d692b874e43f6217047e778f51bed0
Xopher <x@hallofthegods.com>
2026-05-08 03:30:51 -0700

    refactor: update application directory structure and rebuild assets via Vite manifest

---
+++ admin/dist/js/lead-magnet.api-C47oKjtq.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-PfJCU2Kd.js";const s=()=>e.get("get_hustle_modules");export{s as g};
--- admin/dist/js/lead-magnet.api-DCelaSr4.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-BVhXC5qL.js";const s=()=>e.get("get_hustle_modules");export{s as g};
