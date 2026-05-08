7d5e8f890b389d59ebc2526322334ad8c53a2b1e
Xopher <x@hallofthegods.com>
2026-05-08 07:54:10 -0700

    chore: update build assets and vite manifest for admin panel modules

--- admin/dist/js/lead-magnet.api-C47oKjtq.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-PfJCU2Kd.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/lit-lamp.api-D52HScxB.js
@@ -0,0 +1,1 @@
+import{i as t}from"./index-D0A8VRrS.js";const n=async()=>t.get("get_system_info"),o=async()=>t.get("get_wp_info"),g=async s=>t.post("get_logs",s||{}),r=async()=>t.get("get_files"),a=async()=>t.get("get_cron_jobs");export{o as a,g as b,r as c,a as d,n as g};
