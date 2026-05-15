68c83ab0c5c304823ac67e0e4149a6b1c9b6246c
Xopher <x@hallofthegods.com>
2026-05-14 18:43:00 -0700

    chore: update production build assets and manifest in admin directory

---
+++ admin/dist/js/xp-jobs-C1FZMTUV.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-BcIAL___.js";import{c5 as t,c7 as e}from"./vendor-DwAse6UF.js";import"./vendor-core-C4-i1vzd.js";import"./vendor-vuetify-VspZUA1D.js";import"./vendor-three-DbgnMstn.js";import"./vendor-echarts-DoydIvwX.js";const r={},c={class:"xp-index"};function n(s,a){return t(),e("div",c," I am an index ")}const x=o(r,[["render",n]]);export{x as default};
