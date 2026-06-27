b2f81e8c899b05d11995b42151a0da17f0f1cee9
Xopher <x@hallofthegods.com>
2026-06-26 22:40:59 -0700

    refactor: update admin build assets by rebuilding and replacing JavaScript bundles in manifest.json

---
+++ admin/dist/js/lead-magnet.api-CbZ16EIS.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-COq0jKHc.js";const s=()=>e.get("get_hustle_modules");export{s as g};
--- admin/dist/js/lead-magnet.api-nSBxwfwp.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BJ1n9h5J.js";const s=()=>e.get("get_hustle_modules");export{s as g};
