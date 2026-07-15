32a3dc73301d81dbad2bec5cbd5c1af8fffdd732
Xopher <x@hallofthegods.com>
2026-07-14 18:08:14 -0700

    chore: regenerate admin build assets and update manifest mappings

--- admin/dist/js/lead-magnet.api--YvYkGwC.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C9kUF5is.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/lead-magnet.api-DkvtrYzV.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DW6_HFP9.js";const s=()=>e.get("get_hustle_modules");export{s as g};
