4006776b68236b57a4ec349da076d2cc495f9f3e
Xopher <x@hallofthegods.com>
2026-07-20 17:14:31 -0700

    chore: update Vite build manifest and refresh hashed chunk files

--- admin/dist/js/lead-magnet.api-DOokjk-v.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-YHNmkvHT.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/lead-magnet.api-QipCITAT.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CJzkmiUy.js";const s=()=>e.get("get_hustle_modules");export{s as g};
