15faf93326cc1438813559a803e7a5a9a22d177a
Xopher <x@hallofthegods.com>
2026-07-25 02:47:18 -0700

    build: refresh and update admin distribution assets via Vite manifest

--- admin/dist/js/lead-magnet.api-Bhr2G9FL.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Dd-9EXIS.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/lead-magnet.api-DWs_haFW.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C3RNR2OC.js";const s=()=>e.get("get_hustle_modules");export{s as g};
