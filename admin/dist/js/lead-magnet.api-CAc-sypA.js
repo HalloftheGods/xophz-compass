71b4db72c63295449ce7fc50521e46359c3580a9
Xopher <x@hallofthegods.com>
2026-08-09 03:40:58 -0700

    feat: implement Questbook organizations management and update admin route assets

--- admin/dist/js/lead-magnet.api-CAc-sypA.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./index-B3Shq3of.js";const s=()=>e.get("get_hustle_modules");export{s as g};
---
+++ admin/dist/js/lead-magnet.api-DJBGT1vf.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-CI4i5azS.js";const s=()=>e.get("get_hustle_modules");export{s as g};
